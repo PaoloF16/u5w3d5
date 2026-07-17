2560514b9c1640a6fe4ed6fe3d47a4aa2259ff2f
PaoloF16 <paolo.cafis@gmail.com>
2026-07-17 10:19:11 +0200

    created entities

---
+++ src/main/java/PaoloF16/u5w3d5/entities/User.java
@@ -0,0 +1,52 @@
+package PaoloF16.u5w3d5.entities;
+
+import PaoloF16.u5w3d5.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@JsonIgnoreProperties({"password", "authorities", "enabled", "accountNonExpired", "credentialsNonExpired", "accountNonLocked"})
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = Role.USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+}
---
+++ src/main/java/PaoloF16/u5w3d5/enums/Role.java
@@ -0,0 +1,5 @@
+package PaoloF16.u5w3d5.enums;
+
+public enum Role {
+    USER, EVENT_MANAGER, ADMIN
+}
