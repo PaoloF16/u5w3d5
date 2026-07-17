2afc2290db011fe0a3c77e6d61b4b6022434ceb7
PaoloF16 <paolo.cafis@gmail.com>
2026-07-17 10:03:59 +0200

    start repo

---
+++ src/test/java/PaoloF16/u5w3d5/U5w3d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package PaoloF16.u5w3d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
